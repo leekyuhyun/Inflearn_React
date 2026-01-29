09765cf8919b588ad09ff9b0f85ddb5e90447ecf
HyunGu <lhjjhg@naver.com>
2026-01-29 17:50:47 +0900

    [Docs & feat] 01월 29일 스터디 정리 (정현구)

---
+++ 1weeks - JavaScript 기본/정현구-1weeks/예제코드/chapter05.js
@@ -0,0 +1,30 @@
+// 1. Number Type 
+let num1 = 28;
+let num2 = 1.5;
+let num3 = -23;
+    // console.log(num1+ num2);
+    // console.log(num1- num2);
+    // console.log(num1/ num2);
+    // console.log(num1% num2);
+
+let inf = Infinity;
+let nan = NaN; // not a number 
+
+// 2. String type 
+let myname = "정현구";
+let myLocation ="안산시"
+let introduce = myname + myLocation;
+
+let introduceText = `${myname}은 ${myLocation}에 거주합니다.`
+// console.log(introduceText);
+
+// 3. Boolean Type 
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null type 
+let empty = null;
+
+// 5. Undefined Type 
+let none; // 초기화 값 없음 
+
---
+++ 1weeks - JavaScript 기본/정현구-1weeks/예제코드/chapter07.js
@@ -0,0 +1,32 @@
+// 1. 대입 연산자 
+let var1 = 1;
+
+// 2. 산술 연산자 
+let num1 = 3+2;
+let num2 = 3-2;
+let num3 = 3*2;
+let num4 = 3/2;
+let num5 = 3%2;
+
+// 3. 복합 대입 연산자 
+let num7 = 10;
+num7 += 20;
+
+// 4. 증감 연산자 
+let num8 =10;
+num8++; //후위 연산자 
+++num8; //전위 연산자 
+
+// 5. 논리 연산자 
+let or = true || false;
+
+let and = true && false;
+
+let not = !true;
+
+// 6. 비교 연산자 
+let comp1 = 1 === 2; // == 두개만 쓰면 값의 자료형은 비교 불가 
+let comp2 = 1 !==2;  // 3번 써야지 자료형까지 봄 
+
+let comp3 = 1 > 2;
+
